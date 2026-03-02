46dc4b6707405c88cc292b78143caf007f9f725e
KyeongMinBae <reaaaaaaal@gmail.com>
2026-03-02 17:41:56 +0900

    91. 문제와 풀이

---
+++ src/collection/compare/test/Suit.java
@@ -0,0 +1,23 @@
+package collection.compare.test;
+
+public enum Suit {
+    SPADE("♠"), //스페이드(♠)
+    HEART("♥"), //하트(♥)
+    DIAMOND("◆"), //다이아몬드(◆)
+    CLUB("♣"); //클로버(♣)
+
+
+    private String icon;
+
+    Suit(String icon) {
+        this.icon = icon;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+}
